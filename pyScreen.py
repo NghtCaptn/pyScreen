6db71f7b05a3131a5599332a77e91f04445085d9
NghtCaptn <35981607+NghtCaptn@users.noreply.github.com>
2018-11-03 00:36:58 -0400

    Updated command usage.
    Added required and optional parameters along with changing the unique ID numbers. Added new features, user can choose between

--- pyScreen.py
+++ pyScreen.py
@@ -1,4 +1,4 @@
-import subprocess, time
+import subprocess, time, argparse, uuid
 '''
 MIT License
 
@@ -22,6 +22,13 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
+
+parser = argparse.ArgumentParser(prog='pyScreen.py', description='Script that generates screenshots for a video file using ffmpeg', usage='%(prog)s FILE PATH -t 1 or 2 -n for number of screen shots')
+parser.add_argument('location', metavar='FULL PATH TO LOCATION', help='File Location')
+parser.add_argument('-l', metavar='FULL PATH TO 2ND SOURCE FILE', help='2nd File Location',)
+parser.add_argument('-t', help='denotes which config you want, 1 for single source 2 for double source', required=True)
+parser.add_argument('-n', help='number of screen shots for single source', required=True)
+
 print("**********************************************************************")
 print("*                            pyScreen 0.1                            *")
 print("**********************************************************************")
@@ -31,23 +38,48 @@ print("**********************************************************************")
 define screen shot function
 '''
 
-def screenShot(source, encode):
-	i = 1
+def single(fileLocation, x):
+	i = 0
 	j = 10
-	while i <= 6:
-		ts = round(time.mktime(time.localtime()))
-		sourceScreen = subprocess.check_call("ffmpeg -ss 00:{3}:00 -t 1 -i {0} -vframes 1 source-{2}-{1}.png".format(source, ts, i, j))
-		encodeScreen = subprocess.check_call("ffmpeg -ss 00:{3}:00 -t 1 -i {0} -vframes 1 encode-{2}-{1}.png".format(encode, ts, i, j))
+	x = int(x)
+	y = 0
+	for i in range(x):
+		if j <= 60:
+			j = j - 60
+			y = 1
+		ts = str(uuid.uuid4().int)[:4]
+		screen = subprocess.check_call("ffmpeg -ss {4}:{3}:00 -t 1 -i \"{0}\" -vframes 1 frame-{2}-{1}.png".format(fileLocation, ts, i, j, y))
+		i += 1
+		j += 5
+	print("Screenshots generated Successfully!")
+
+def encSor(source, encode, x):
+	i = 0
+	j = 10
+	x = int(x)
+	y = 0
+	for i in range(x):
+		if j <= 60:
+			j = j - 60
+			y = 1
+		ts = str(uuid.uuid4().int)[:4]
+		sourceScreen = subprocess.check_call("ffmpeg -ss {4}:{3}:00 -t 1 -i \"{0}\" -vframes 1 source-{2}-{1}.png".format(source, ts, i, j, y))
+		encodeScreen = subprocess.check_call("ffmpeg -ss {4}:{3}:00 -t 1 -i \"{0}\" -vframes 1 encode-{2}-{1}.png".format(encode, ts, i, j, y))
 		i += 1
 		j += 5
 	print("Screenshots Generated Successfully!")
 
-source = input("Enter your Source Location: ")
-type(source)
-encode = input("Enter your Encode Location: ")
-type(encode)
+args = parser.parse_args()
 
-screenShot(source, encode)
+
+location1 = args.location
+if args.t == "1":
+	x = args.n
+	single(location1, x)
+	print("Screen Shots for single source created")
+
+if args.t == "2":
+	encSor(location1, args.l)
 
 
 
